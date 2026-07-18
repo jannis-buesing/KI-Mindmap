ebf3d904355ab4a547f625b0e2ba0df58ee3e51e
schwanniii <jannis.buesing@gmx.de>
2026-07-18 20:33:42 +0200

    vite.config. fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import babel from '@rolldown/plugin-babel';
+import babel from 'vite-plugin-babel';
 
 const reactCompilerPreset = require('babel-plugin-react-compiler');
 
